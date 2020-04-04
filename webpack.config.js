6fc3a29d064d064e45e708f3844040745bfcdefc
Denis Shevchenko <DAlShevchenko@sberbank.ru>
2020-04-04 19:05:47 +0300

    Configure webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin }= require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '../public'),
+    path: path.join(__dirname, './public'),
     filename: '[hash].js'
   },
-  rules: [
-    {
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }
-  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }, 
   resolve: {
     extensions: ['.js', '.jsx', '.ts']
   },
+  // devServer: {
+  //   contentBase: './public',
+  // },
   plugins: [
     new HtmlWebpackPlugin({
       inject: 'body',
       template: './index.ejs'
-    })
+    }),
+    new CleanWebpackPlugin()
   ]
 }
