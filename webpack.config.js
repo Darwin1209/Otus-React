7ee1948109de1bed432b36e3c45520d197caf0ab
Denis Shevchenko <DAlShevchenko@sberbank.ru>
2020-04-04 18:46:35 +0300

    Create webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '../public'),
+    filename: '[hash].js'
+  },
+  rules: [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './index.ejs'
+    })
+  ]
 }
